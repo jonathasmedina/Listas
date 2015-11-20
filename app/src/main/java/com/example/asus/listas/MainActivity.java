64c3d6c21282b1078b06f10bd251d2c23592ff3c
Jonathas Leontino Medina <jonathas.medina@ifms.edu.br>
2015-11-20 13:41:40 -0400

    commit

--- app/src/main/java/com/example/asus/listas/MainActivity.java
+++ app/src/main/java/com/example/asus/listas/MainActivity.java
@@ -17,13 +17,13 @@ public class MainActivity extends ListActivity {
     private ArrayAdapter<String> meuAdaptador;
 
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
+ protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-       // setContentView(R.layout.activity_main);
+        // setContentView(R.layout.activity_main);
 
-     meuAdaptador  = new ArrayAdapter(this,android.R.layout.simple_list_item_1, CORES);
+        meuAdaptador  = new ArrayAdapter(this,android.R.layout.simple_list_item_1, CORES);
         setListAdapter(meuAdaptador);
-}
+    }
 
     @Override
     protected void onListItemClick (ListView l, View v, int posicao, long id)
